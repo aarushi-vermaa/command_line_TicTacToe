299d9cd3b9911dced8557cee3a51065cba920e71
sdonatileach <sdona@DESKTOP-R9F496I.localdomain>
2022-04-20 18:40:07 -0400

    trying out monkeypatch

--- test_game.py
+++ test_game.py
@@ -1,15 +1,19 @@
 """Test Tic Tac Toe classes and functions."""
 import pytest
-from tic_tac_toe_game.py import tic_tac_toe
+import tic_tac_toe_game as tttg
 
+game = tttg.tic_tac_toe()
 
-def test_tic_tac_toe(monkeypatch):
+def test_enter_postition(monkeypatch):
     """Test Tic Tac Toe."""
-    game = tic_tac_toe()
-    # monkeypatch the "input" function, so that it returns "Mark".
-    # This simulates the user entering "Mark" in the terminal:
+    # This simulates the user entering "1" in the terminal:
     monkeypatch.setattr('builtins.input', lambda _: "1")
-
+    responses = iter(['"Position Occupied. Please Try Again.',\
+        'Invalid input. Please enter a number between 1 and 9',\
+            'Invalid input. Please enter a number between 1 and 9'])
+    monkeypatch.setattr('builtins.input', lambda msg: next(responses))
     # go about using input() like you normally would:
-    i = input("Please enter the position:")
-    assert i == "1"
+    monkeypatch.setattr('builtins.input', lambda _: "N")
+
+
+    # monkeypatch.setattr('builtins.input', lambda _: "1")
