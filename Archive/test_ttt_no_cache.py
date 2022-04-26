34ddb1556884322134063829099bef3afd6e2aa3
Aarushi Verma <aarushi.verma@duke.edu>
2022-04-26 13:10:31 -0400

    move old test file to archive

---
+++ Archive/test_ttt_no_cache.py
@@ -0,0 +1,124 @@
+"""Test Tic Tac Toe classes and functions."""
+import pytest
+from unittest import mock
+from unittest.mock import patch
+import TicTacToe_no_cache as ttt
+import numpy as np
+
+# Initialize class for testing
+game = ttt.tic_tac_toe()
+
+
+def test_game_board():
+    """Test Tic Tac Toe.
+
+    This function tests if the board is created correctly.
+    """
+    assert game.board.shape == (3, 3)
+    assert np.array_equiv(
+        game.board, np.array([["", "", ""], ["", "", ""], ["", "", ""]])
+    )
+
+
+def test_ask_question(monkeypatch):
+    """Test the ask_question function.
+
+    This function tests if the ask question function
+    correctly accepts a correct input entered by the user.
+    """
+    # This simulates the user entering "1" in the terminal:
+    inputs = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    for response in inputs:
+        monkeypatch.setattr("builtins.input", lambda _: response)
+        # go about using input() like you normally would:
+        a = game.ask_question(response, [str(i) for i in game.place_maps.keys()])
+        assert a == response
+
+
+# def test_invalid_input(monkeypatch):
+#     """Test invalid input throwing error."""
+#     monkeypatch.setattr(
+#         "builtins.input",
+#         lambda _: "INVALID INPUT. Please respond with one of the following: ",
+#     )
+#     # go about using input() like you normally would:
+#     input = ["b", "1"]
+#     for j in input:
+#         b = game.ask_question(j, [str(i) for i in game.place_maps.keys()])
+#         assert (
+#             b
+#             == "INVALID INPUT. Please respond with one of the following: ['1', '2', '3', '4', '5', '6', '7', '8', '9']"
+#         )
+#         exit()
+
+
+def test_check_repeating_letter():
+    """Test the check_repeating_letter function.
+
+    This function tests if an array has 3 of the same
+     letters the check_repeating letter function returns True,
+     else False.
+    """
+    assert game.check_repeating_letter(np.array(["X", "X", ""])) == False
+    assert game.check_repeating_letter(np.array(["X", "X", "X"])) == True
+
+
+def test_check_game_over():
+    """Test the check_game_over function.
+
+    This function tests if the check_game_over function correctly identifies
+    a game is over or not. It returns True when game is over else False.
+    """
+    game.board = np.array([["X", "X", "X"], ["", "", ""], ["", "", ""]])
+    assert game.check_game_over() == True
+    game.board = np.array([["X", "", "X"], ["", "O", "O"], ["X", "", ""]])
+    assert game.check_game_over() == False
+    game.board = np.array([["X", "", ""], ["X", "O", "O"], ["X", "", ""]])
+    assert game.check_game_over() == True
+    game.board = np.array([["X", "O", "X"], ["X", "O", "X"], ["O", "X", "O"]])
+    assert game.check_game_over() == True
+
+
+def test_winning_player():
+    """Test the winning player function.
+
+    This functions tests whether the correct winning player is returned
+    based on the winning mark or if the game has neded in a Tie.
+    """
+    if game.game_mode == "1":
+        game.board = np.array([["X", "O", "O"], ["X", "O", "X"], ["O", "X", "O"]])
+        assert game.winning_player() == "Player 2"
+        game.board = np.array([["X", "", ""], ["X", "O", "O"], ["X", "", ""]])
+        assert game.winning_player() == "Player 1"
+        game.board = np.array([["X", "O", "X"], ["X", "O", "X"], ["O", "X", "O"]])
+        assert game.winning_player() == "Tie"
+    else:
+        game.board = np.array([["X", "O", "O"], ["X", "O", "X"], ["O", "X", "O"]])
+        assert game.winning_player() == "Computer"
+        game.board = np.array([["X", "", ""], ["X", "O", "O"], ["X", "", ""]])
+        assert game.winning_player() == "User"
+        game.board = np.array([["X", "O", "X"], ["X", "O", "X"], ["O", "X", "O"]])
+        assert game.winning_player() == "Tie"
+
+
+def test_computer_play():
+    """Test the 31 lines of code dedicated to the computer's play.
+
+    This function tests multiple functions when the user is playing
+    against the Computer. It checks if the correct move is returned
+    based on the highest score calculated using the minimax function.
+    """
+    if game.game_mode == "2":
+        game.board = np.array([["O", "X", "X"], ["", "X", ""], ["", "O", "O"]])
+        assert game.computer_play() == 7
+    else:
+        pass
+
+
+# def test_play_again(monkeypatch):
+#     inputs = (['Y', 'N'])
+#     for response in inputs:
+#         monkeypatch.setattr('builtins.input', lambda _: response)
+#         # go about using input() like you normally would:
+#         a = game.play_again()
+#         assert a == response
